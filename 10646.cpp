974e73791616a2c54ceea9e7e9eea58caa4b59c9
Edwin Mahecha <edwmapa@gmail.com>
2018-01-29 22:55:18 -0500

    What is the card? - Problem submission

--- 10646.cpp
+++ 10646.cpp
@@ -2,31 +2,34 @@
 
 using namespace std;
 
+int value(string card){
+	return (card[0]>='2' && card[0]<='9')?card[0]-'0': 10;
+}
+
 int main(){
-	freopen("test.in","r",stdin);
-	freopen("test.out","w",stdout);
-	int t;
-	cin>>t;
-	for (int i = 1;i<=t;i++){
-		string leftPile[26], hand[26];
-		int y = 0, currentPile = 25;
-		for(int j = 0; j<26; j++)cin>>leftPile[j];
-		for(int j = 0; j<26; j++)cin>>hand[j];
+	//freopen("test.in","r",stdin);
+	//freopen("test.out","w",stdout);
+	int n;
+	cin>>n;
+	for (int c = 1 ; c<=n;c++){
+		string cards[52];
+		for(int i = 0;i<52;i++){
+			cin>>cards[i];
+		}
 		
-		/*cout<<endl<<"pile\n";
-		for(int j = 0 ; j<26;j++){
-			cout<<leftPile[j]<<" ";
-		}
-		cout<<endl;*/
-		while(currentPile>=0){
-			int td;
-			//cout<<">> "<<leftPile[currentPile];
-			if(!(leftPile[currentPile][0]>='2' && leftPile[currentPile][0]<='9'))td = 10;
-			else td = leftPile[currentPile][0]-'0';
-			//cout<<" X> "<<td<<endl;
-			y+=td;
-			if(currentPile-((10-td)+1))currentPile-=((10-td)+1);
-		}
-		cout<<"Case "<<i<<": "<<hand[y%26]<<endl;
+		int top = 26,y=0;
+		//cout<<cards[top]<<endl;
+		for(int i=0;i<3;i++){
+			int x = value(cards[top]);
+			y+=x;
+			top-=(10-x)+1;
+			//cout<<cards[top]<<" "<<top<<" x>"<<x<<endl;
+		}		
+		
+		//cout<<cards[top];
+		cout<<"Case "<<c<<": ";
+		if(y-1<=top)cout<<cards[y-1]<<'\n';
+		else cout<<cards[27+(y-2-top)]<<'\n';
+		
 	}
 }
